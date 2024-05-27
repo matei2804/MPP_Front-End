bf03089d991185ff0328dc85e5c3d6fa7bec429d
925-Mihai-Matei <teo28042003@gmail.com>
2024-05-27 21:13:49 +0300

    + dockerfile

--- src/config.jsx
+++ src/config.jsx
@@ -1,5 +1,6 @@
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://mpp-backend.up.railway.app';
 
-//'https://mpp-backend.up.railway.app'
+//http://localhost:8080
+//https://mpp-backend.up.railway.app/
 
 export default BASE_URL;
